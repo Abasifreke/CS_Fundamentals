1ab0748b884293e74d98bfd3026f6c130ee2ece7
Abasifreke James <jamesfreky@gmail.com>
2023-12-21 10:42:29 -0500

    Time: 3 ms (94.19%) | Memory: 40.9 MB (21.02%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.java
+++ 37-sudoku-solver/sudoku-solver.java
@@ -1,135 +1,102 @@
 class Solution {
-    // box size
-    int n = 3;
-    // row size
-    int N = n * n;
+  // box size
+  int n = 3;
+  // row size
+  int N = n * n;
 
-    int [][] rows = new int[N][N + 1];
-    int [][] columns = new int[N][N + 1];
-    int [][] boxes = new int[N][N + 1];
+  int [][] rows = new int[N][N + 1];
+  int [][] columns = new int[N][N + 1];
+  int [][] boxes = new int[N][N + 1];
 
-    char[][] board;
+  char[][] board;
 
-    boolean sudokuSolved = false;
+  boolean sudokuSolved = false;
 
-    public boolean couldPlace(int d, int row, int col) {
-        /*
-        Check if one could place a number d in (row, col) cell
-        */
-        int idx = (row / n ) * n + col / n;
-        return rows[row][d] + columns[col][d] + boxes[idx][d] == 0;
+  public boolean couldPlace(int d, int row, int col) {
+    /*
+    Check if one could place a number d in (row, col) cell
+    */
+    int idx = (row / n ) * n + col / n;
+    return rows[row][d] + columns[col][d] + boxes[idx][d] == 0;
+  }
+
+  public void placeNumber(int d, int row, int col) {
+    /*
+    Place a number d in (row, col) cell
+    */
+    int idx = (row / n ) * n + col / n;
+
+    rows[row][d]++;
+    columns[col][d]++;
+    boxes[idx][d]++;
+    board[row][col] = (char)(d + '0');
+  }
+
+  public void removeNumber(int d, int row, int col) {
+    /*
+    Remove a number that didn't lead to a solution
+    */
+    int idx = (row / n ) * n + col / n;
+    rows[row][d]--;
+    columns[col][d]--;
+    boxes[idx][d]--;
+    board[row][col] = '.';
+  }
+
+  public void placeNextNumbers(int row, int col) {
+    /*
+    Call backtrack function in recursion
+    to continue to place numbers
+    till the moment we have a solution
+    */
+    //If we're in the last cell
+    // that means we have the solution
+    if ((col == N - 1) && (row == N - 1)) {
+      sudokuSolved = true;
     }
+    // if not yet
+    else {
+      //If we're at the end of the row
+      // go to the next row
+      if (col == N - 1) backtrack(row + 1, 0);
+        // go to the next column
+      else backtrack(row, col + 1);
+    }
+  }
 
-    public boolean couldPlaceOnlyOne( int row, int col) {
-        /*
-        Check if one could only place one number in (row, col) cell
-        */
-        int place = 0, count = 0;
-        for (int d = 1; d < 10; d++) {
-            int idx = (row / n ) * n + col / n;
-            if (rows[row][d] + columns[col][d] + boxes[idx][d] == 0) {
-                count += 1;
-                place = d;
-            }
+  public void backtrack(int row, int col) {
+    /*
+    Backtracking
+    */
+    //If the cell is empty
+    if (board[row][col] == '.') {
+      // iterate over all numbers from 1 to 9
+      for (int d = 1; d < 10; d++) {
+        if (couldPlace(d, row, col)) {
+          placeNumber(d, row, col);
+          placeNextNumbers(row, col);
+          // If sudoku is solved, there is no need to backtrack
+          // since the single unique solution is promised
+          if (!sudokuSolved) removeNumber(d, row, col);
         }
-        if (count == 1) {
-            placeNumber(place, row, col);
+      }
+    }
+    else placeNextNumbers(row, col);
+  }
+
+  public void solveSudoku(char[][] board) {
+    this.board = board;
+
+    // init rows, columns, and boxes
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < N; j++) {
+        char num = board[i][j];
+        if (num != '.') {
+          int d = Character.getNumericValue(num);
+          placeNumber(d, i, j);
         }
-        return false;
+      }
     }
-
-    public void placeNumber(int d, int row, int col) {
-        /*
-        Place a number d in (row, col) cell
-        */
-        int idx = (row / n ) * n + col / n;
-
-        rows[row][d]++;
-        columns[col][d]++;
-        boxes[idx][d]++;
-        board[row][col] = (char)(d + '0');
-    }
-
-    public void removeNumber(int d, int row, int col) {
-        /*
-        Remove a number which didn't lead to a solution
-        */
-        int idx = (row / n ) * n + col / n;
-        rows[row][d]--;
-        columns[col][d]--;
-        boxes[idx][d]--;
-        board[row][col] = '.';
-    }
-
-    public void placeNextNumbers(int row, int col) {
-        /*
-        Call backtrack function in recursion
-        to continue to place numbers
-        till the moment we have a solution
-        */
-        // if we're in the last cell
-        // that means we have the solution
-        if ((col == N - 1) && (row == N - 1)) {
-            sudokuSolved = true;
-        }
-        // if not yet
-        else {
-            // if we're in the end of the row
-            // go to the next row
-            if (col == N - 1) backtrack(row + 1, 0);
-            // go to the next column
-            else backtrack(row, col + 1);
-        }
-    }
-
-    public void backtrack(int row, int col) {
-        /*
-        Backtracking
-        */
-        // if the cell is empty
-        if (board[row][col] == '.') {
-            // iterate over all numbers from 1 to 9
-            for (int d = 1; d < 10; d++) {
-                if (couldPlace(d, row, col)) {
-                    placeNumber(d, row, col);
-                    placeNextNumbers(row, col);
-                    // if sudoku is solved, there is no need to backtrack
-                    // since the single unique solution is promised
-                    if (!sudokuSolved) removeNumber(d, row, col);
-                }
-            }
-        } else placeNextNumbers(row, col);
-    }
-
-    public void solveSudoku(char[][] board) {
-        this.board = board;
-
-        // init rows, columns and boxes
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                char num = board[i][j];
-                if (num != '.') {
-                    int d = Character.getNumericValue(num);
-                    placeNumber(d, i, j);
-                }
-            }
-        }
-        while (true) {
-            int flag = 0;
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
-                    if (board[i][j] == '.') {
-                        if (couldPlaceOnlyOne(i, j) == true) {
-                            flag = 1;
-                        }
-                    }
-                }
-            }
-            if (flag == 0) {
-                break;
-            }
-
-        }
-        backtrack(0, 0);
-    }
+    backtrack(0, 0);
+  }
 }
