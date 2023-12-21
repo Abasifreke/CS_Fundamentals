2d8c0c2cb7bfb4de168b51821846829c4ee403fe
Abasifreke James <jamesfreky@gmail.com>
2023-12-21 10:40:18 -0500

    Time: 16 ms (9.98%) | Memory: 44.9 MB (5.13%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.java
+++ 37-sudoku-solver/sudoku-solver.java
@@ -1,98 +1,89 @@
 class Solution {
-    int done_count=0;
-    Map<Integer,Set<Integer>> rows = new HashMap<>();
-    Map<Integer,Set<Integer>> cols = new HashMap<>();
-    Map<Integer,Set<Integer>> nines = new HashMap<>();
-    int done = 0;
-    char[][] ans;
+    private int[][] result = new int[9][9];
+    private boolean[][] modifiable = new boolean[9][9];
 
-    public void copy(char[][] ans,char[][] board)
-    {
-        for(int i=0;i<board.length;i++)
-        {
-            for(int j=0;j<board.length;j++)
-            {
-                ans[i][j] = board[i][j];
-            }
-        }
-    }
-
-    public void solve(char[][] board,int ind)
-    {
-        if(done == 1)
-        {
-            return;
-        }
-        if(ind == board.length)
-        {
-            copy(ans,board);
-            done = 1;
-            return;
-        }
-        Set<Integer> s1 = rows.getOrDefault(ind,new HashSet<>());
-        for(int j=0;j<board[0].length;j++)
-        {
-            Set<Integer> s2 = cols.getOrDefault(j,new HashSet<>());
-            Set<Integer> s3 = nines.getOrDefault(3*(ind/3)+(j/3),new HashSet<>());
-            if(board[ind][j]=='.')
-            { 
-                for(int p=1;p<=9;p++)
-                {
-                    if(!s1.contains(p) && !s2.contains(p) && !s3.contains(p))
-                    {
-                        s1.add(p);
-                        s2.add(p);
-                        s3.add(p);
-                        board[ind][j] = (char)(p+'0');
-                        rows.put(ind,s1);
-                        cols.put(j,s2);
-                        nines.put(3*(ind/3)+(j/3),s3);
-                        solve(board,ind);
-                        s1.remove(p);
-                        s2.remove(p);
-                        s3.remove(p);
-                        rows.put(ind,s1);
-                        cols.put(j,s2);
-                        nines.put(3*(ind/3)+(j/3),s3);
-                        board[ind][j] = '.';
-                    }
-                }
-                if(board[ind][j]=='.')
-                {
-                    return;
-                }
-            }
-        }
-        solve(board,ind+1);
-    }
     public void solveSudoku(char[][] board) {
-        ans = new char[board.length][board.length];
-        for(int i=0;i<board.length;i++)
-        {
-            for(int j=0;j<board.length;j++)
-            {
-                Set<Integer> s1 = rows.getOrDefault(i,new HashSet<>());
-                Set<Integer> s2 = cols.getOrDefault(j,new HashSet<>());
-                Set<Integer> s3 = nines.getOrDefault(3*(i/3)+(j/3),new HashSet<>());
+        markModifiable(board);
+        recurse(0);
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                board[i][j] = Integer.toString(result[i][j]).charAt(0);
+            }
+        }
+    }
 
-                if(board[i][j]!='.')
-                {
-                    s1.add(board[i][j]-'0');
-                    s2.add(board[i][j]-'0');
-                    s3.add(board[i][j]-'0');
-                    rows.put(i,s1);
-                    cols.put(j,s2);
-                    nines.put(3*(i/3)+(j/3),s3);
+    private void markModifiable(char[][] board) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                modifiable[i][j] = (board[i][j] == '.');
+                if (!modifiable[i][j]) {
+                    result[i][j] = Integer.parseInt(String.valueOf(board[i][j]));
                 }
             }
         }
-        solve(board,0);
-        for(int i=0;i<ans.length;i++)
-        {
-            for(int j=0;j<ans.length;j++)
-            {
-                board[i][j] = ans[i][j];
+    }
+
+    private boolean recurse(int idx) {
+        if (idx == 81) {
+            return true;
+        }
+
+        int row = idx / 9;
+        int column = idx % 9;
+
+        for (int candidate = 1; candidate < 10; candidate++) {
+            if (modifiable[row][column]) {
+                result[row][column] = candidate;
+                if (isValid(row, column) && recurse(idx + 1)) {
+                    return true;
+                }
+            } else {
+                return recurse(idx + 1);
             }
         }
+        result[row][column] = 0;
+        return false;
+    }
+
+    private boolean isValid(int i, int j) {
+        return isValidRow(i) && isValidColumn(j) && isValidBlock(i, j);
+    }
+
+    private boolean isValidRow(int i) {
+        boolean[] seen = new boolean[10];
+        for (int j = 0; j < 9; j++) {
+            int value = result[i][j];
+            if (seen[value] && value != 0) {
+                return false;
+            }
+            seen[value] = true;
+        }
+        return true;
+    }
+
+    private boolean isValidColumn(int j) {
+        boolean[] seen = new boolean[10];
+        for (int i = 0; i < 9; i++) {
+            int value = result[i][j];
+            if (seen[value] && value != 0) {
+                return false;
+            }
+            seen[value] = true;
+        }
+        return true;
+    }
+
+    private boolean isValidBlock(int i, int j) {
+        boolean[] seen = new boolean[10];
+        for (int row = (i / 3) * 3; row < (i / 3) * 3 + 3; row++) {
+            for (int column = (j / 3) * 3; column < (j / 3) * 3 + 3; column++) {
+                int value = result[row][column];
+                if (seen[value] && value != 0) {
+                    return false;
+                }
+                seen[value] = true;
+           }
+        }
+        return true;
     }
 }
