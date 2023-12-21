7dee741ac5c20bb3c0cd9813b724605f427b88fb
Abasifreke James <jamesfreky@gmail.com>
2023-12-21 10:39:16 -0500

    Time: 11 ms (37.79%) | Memory: 40.9 MB (21.02%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.java
+++ 37-sudoku-solver/sudoku-solver.java
@@ -1,72 +1,72 @@
 class Solution {
-    Map<Integer, Set<Character>> row = new HashMap<>();
-    Map<Integer, Set<Character>> col = new HashMap<>();
-    Map<Integer, Set<Character>> box = new HashMap<>();
-
     public void solveSudoku(char[][] board) {
-        int m = board.length;
-        int n = board[0].length;
+        if(board == null || board.length == 0)
+            return;
+        solve(board);
+    }
 
-        for (int i = 0; i <= 9; i++) {
-            row.putIfAbsent(i, new HashSet<Character>());
-            col.putIfAbsent(i, new HashSet<Character>());
-            box.putIfAbsent(i, new HashSet<Character>());
+   private boolean solve(char[][] board) {
+    // your code goes here
+    int row = 0, col = 0;
+    while(row < 9 && col < 9){
+      // iterate through row and check for empty spot. When found, put 
+      
+      if(board[row][col] == '.'){
+        for(char choice = '1'; choice <= '9'; choice++){
+          if(isValid(board, row, col, choice)){
+            board[row][col] = choice;
+            if(solve(board)){
+              return true;
+            }else{
+              board[row][col] = '.';
+            }
+          }
         }
+        return false;
+      }
 
-        List<int[]> list = new ArrayList<>();
+      col++;
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (board[i][j] != '.') {
-                    char k = board[i][j];
-
-                    row.putIfAbsent(i, new HashSet<Character>());
-                    //if (row.get(i).contains(k)) return false;
-                    row.get(i).add(k);
-
-                    col.putIfAbsent(j, new HashSet<Character>());
-                    //if (col.get(j).contains(k)) return false;
-                    col.get(j).add(k);
-
-                    int index = (i/3)*3 + j/3; // box index
-
-                    box.putIfAbsent(index, new HashSet<Character>());
-                    //if (box.get(index).contains(k)) return false;
-                    box.get(index).add(k);
-                } else {
-                    list.add(new int[]{i, j});
+      if(col == 9){
+        row++;
+        col = 0;
+      }
+    }
+    return true;
+  }
+   public boolean solve1(char[][] board){
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[0].length; j++){
+                if(board[i][j] == '.'){
+                    for(char c = '1'; c <= '9'; c++){//trial. Try 1 through 9
+                        if(isValid(board, i, j, c)){
+                            board[i][j] = c; //Put c for this cell
+                            
+                            if(solve(board))
+                                return true; //If it's the solution return true
+                            else
+                                board[i][j] = '.'; //Otherwise go back
+                        }
+                    }
+                    
+                    return false;
                 }
             }
         }
-
-        helper(board, list, 0);
+        return true;
     }
+  
 
-    boolean helper(char[][] board, List<int[]> list, int index) {
-        if(index == list.size()) return true;
-
-        int[] a = list.get(index);
-        int i = a[0];
-        int j = a[1];
-        int k = (i/3)*3 + j/3; // box index
-
-        for (int q = 1; q <= 9; q++){
-            char p = (char)('0' + q);
-
-            if(!row.get(i).contains(p) && !col.get(j).contains(p) && !box.get(k).contains(p)) {
-                board[i][j] = p;
-                row.get(i).add(p);
-                col.get(j).add(p);
-                box.get(k).add(p);
-
-                if(helper(board, list, index+1)) return true;
-
-                board[i][j] = '.';
-                row.get(i).remove(p);
-                col.get(j).remove(p);
-                box.get(k).remove(p);
-            }       
-        }
-        return false;
-    }
+  
+  private boolean isValid(char[][] board, int row, int col, char c){
+       for(int i = 0; i < 9; i++){
+          if(board[row][i] == c) return false;
+          if(board[i][col] == c) return false;
+          int cRow = (3 * (row/3)) + i%3;
+          int cCol = (3 * (col/3)) + i/3;
+          if(board[cRow][cCol] == c) return false; //check 3*3 block
+       }
+    return true;
+  }
+  
 }
