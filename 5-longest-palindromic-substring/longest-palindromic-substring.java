e58b4d346dea76bfc0d19bebde7aba611f1d4566
Abasifreke James <jamesfreky@gmail.com>
2023-12-21 12:01:29 -0500

    Time: 117 ms (39.49%) | Memory: 46.3 MB (7.69%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,37 +1,34 @@
 class Solution {
     public String longestPalindrome(String s) {
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
         int[] ans = new int[]{0, 0};
         
-        for (int i = 0; i < s.length(); i++) {
-            int oddLength = expand(i, i, s);
-            if (oddLength > ans[1] - ans[0] + 1) {
-                int dist = oddLength / 2;
-                ans[0] = i - dist;
-                ans[1] = i + dist;
-            }
-            
-            int evenLength = expand(i, i + 1, s);
-            if (evenLength > ans[1] - ans[0] + 1) {
-                int dist = (evenLength / 2) - 1;
-                ans[0] = i - dist;
-                ans[1] = i + 1 + dist;
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = true;
+        }
+        
+        for (int i = 0; i < n - 1; i++) {
+            if (s.charAt(i) == s.charAt(i + 1)) {
+                dp[i][i + 1] = true;
+                ans[0] = i;
+                ans[1] = i + 1;
             }
         }
-
+        
+        for (int diff = 2; diff < n; diff++) {
+            for (int i = 0; i < n - diff; i++) {
+                int j = i + diff;
+                if (s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1]) {
+                    dp[i][j] = true;
+                    ans[0] = i;
+                    ans[1] = j;
+                }
+            }
+        }
+        
         int i = ans[0];
         int j = ans[1];
         return s.substring(i, j + 1);
-    }
-    
-    private int expand(int i, int j, String s) {
-        int left = i;
-        int right = j;
-        
-        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
-            left--;
-            right++;
-        }
-
-        return right - left - 1;
     }
 }
