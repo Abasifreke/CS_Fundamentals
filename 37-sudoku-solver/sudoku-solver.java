9b03c67a5bd39619d825519b57d79c40376cdf49
Abasifreke James <jamesfreky@gmail.com>
2023-12-21 10:39:38 -0500

    Time: 25 ms (5.35%) | Memory: 44.5 MB (5.13%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.java
+++ 37-sudoku-solver/sudoku-solver.java
@@ -1,72 +1,98 @@
 class Solution {
-    public void solveSudoku(char[][] board) {
-        if(board == null || board.length == 0)
-            return;
-        solve(board);
-    }
+    int done_count=0;
+    Map<Integer,Set<Integer>> rows = new HashMap<>();
+    Map<Integer,Set<Integer>> cols = new HashMap<>();
+    Map<Integer,Set<Integer>> nines = new HashMap<>();
+    int done = 0;
+    char[][] ans;
 
-   private boolean solve(char[][] board) {
-    // your code goes here
-    int row = 0, col = 0;
-    while(row < 9 && col < 9){
-      // iterate through row and check for empty spot. When found, put 
-      
-      if(board[row][col] == '.'){
-        for(char choice = '1'; choice <= '9'; choice++){
-          if(isValid(board, row, col, choice)){
-            board[row][col] = choice;
-            if(solve(board)){
-              return true;
-            }else{
-              board[row][col] = '.';
+    public void copy(char[][] ans,char[][] board)
+    {
+        for(int i=0;i<board.length;i++)
+        {
+            for(int j=0;j<board.length;j++)
+            {
+                ans[i][j] = board[i][j];
             }
-          }
         }
-        return false;
-      }
-
-      col++;
-
-      if(col == 9){
-        row++;
-        col = 0;
-      }
     }
-    return true;
-  }
-   public boolean solve1(char[][] board){
-        for(int i = 0; i < board.length; i++){
-            for(int j = 0; j < board[0].length; j++){
-                if(board[i][j] == '.'){
-                    for(char c = '1'; c <= '9'; c++){//trial. Try 1 through 9
-                        if(isValid(board, i, j, c)){
-                            board[i][j] = c; //Put c for this cell
-                            
-                            if(solve(board))
-                                return true; //If it's the solution return true
-                            else
-                                board[i][j] = '.'; //Otherwise go back
-                        }
+
+    public void solve(char[][] board,int ind)
+    {
+        if(done == 1)
+        {
+            return;
+        }
+        if(ind == board.length)
+        {
+            copy(ans,board);
+            done = 1;
+            return;
+        }
+        Set<Integer> s1 = rows.getOrDefault(ind,new HashSet<>());
+        for(int j=0;j<board[0].length;j++)
+        {
+            Set<Integer> s2 = cols.getOrDefault(j,new HashSet<>());
+            Set<Integer> s3 = nines.getOrDefault(3*(ind/3)+(j/3),new HashSet<>());
+            if(board[ind][j]=='.')
+            { 
+                for(int p=1;p<=9;p++)
+                {
+                    if(!s1.contains(p) && !s2.contains(p) && !s3.contains(p))
+                    {
+                        s1.add(p);
+                        s2.add(p);
+                        s3.add(p);
+                        board[ind][j] = (char)(p+'0');
+                        rows.put(ind,s1);
+                        cols.put(j,s2);
+                        nines.put(3*(ind/3)+(j/3),s3);
+                        solve(board,ind);
+                        s1.remove(p);
+                        s2.remove(p);
+                        s3.remove(p);
+                        rows.put(ind,s1);
+                        cols.put(j,s2);
+                        nines.put(3*(ind/3)+(j/3),s3);
+                        board[ind][j] = '.';
                     }
-                    
-                    return false;
+                }
+                if(board[ind][j]=='.')
+                {
+                    return;
                 }
             }
         }
-        return true;
+        solve(board,ind+1);
     }
-  
+    public void solveSudoku(char[][] board) {
+        ans = new char[board.length][board.length];
+        for(int i=0;i<board.length;i++)
+        {
+            for(int j=0;j<board.length;j++)
+            {
+                Set<Integer> s1 = rows.getOrDefault(i,new HashSet<>());
+                Set<Integer> s2 = cols.getOrDefault(j,new HashSet<>());
+                Set<Integer> s3 = nines.getOrDefault(3*(i/3)+(j/3),new HashSet<>());
 
-  
-  private boolean isValid(char[][] board, int row, int col, char c){
-       for(int i = 0; i < 9; i++){
-          if(board[row][i] == c) return false;
-          if(board[i][col] == c) return false;
-          int cRow = (3 * (row/3)) + i%3;
-          int cCol = (3 * (col/3)) + i/3;
-          if(board[cRow][cCol] == c) return false; //check 3*3 block
-       }
-    return true;
-  }
-  
+                if(board[i][j]!='.')
+                {
+                    s1.add(board[i][j]-'0');
+                    s2.add(board[i][j]-'0');
+                    s3.add(board[i][j]-'0');
+                    rows.put(i,s1);
+                    cols.put(j,s2);
+                    nines.put(3*(i/3)+(j/3),s3);
+                }
+            }
+        }
+        solve(board,0);
+        for(int i=0;i<ans.length;i++)
+        {
+            for(int j=0;j<ans.length;j++)
+            {
+                board[i][j] = ans[i][j];
+            }
+        }
+    }
 }
