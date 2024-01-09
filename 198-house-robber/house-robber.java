bdc03703b3ffd406079626f6b3e5ca7c03a79528
Abasifreke James <jamesfreky@gmail.com>
2024-01-09 10:29:04 -0500

    Time: 2 ms (5.38%) | Memory: 41 MB (14.16%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -8,16 +8,16 @@ class Solution {
 
         // int oneHouseAgo = nums[1];
         // int twoHousesAgo = nums[0];
-        int[] robbed = new int[nums.length + 1];
+        int[] robbed = new int[nums.length];
         robbed[0] = nums[0];
         robbed[1] = Math.max(nums[0], nums[1]);
 
-        for(int i = 2; i < robbed.length - 1; i++ ){
+        for(int i = 2; i < robbed.length; i++ ){
             robbed[i] = Math.max(nums[i] + robbed[i-2], robbed[i-1]);
             // twoHousesAgo = oneHouseAgo;
             // oneHouseAgo = maxRobbed;
         }
         System.out.println(Arrays.toString(robbed));
-        return Math.max(robbed[robbed.length - 1], robbed[robbed.length-2]);
+        return Math.max(robbed[robbed.length-1], robbed[robbed.length-2]);
     }
 }
