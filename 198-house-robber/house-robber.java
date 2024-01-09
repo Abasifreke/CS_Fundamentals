6e1ae271f267337561607335d4d0a2947713e55a
Abasifreke James <jamesfreky@gmail.com>
2024-01-09 10:31:53 -0500

    Time: 0 ms (100.00%) | Memory: 41.4 MB (7.48%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -6,18 +6,22 @@ class Solution {
             return Math.max(nums[0], nums[1]);
         }
 
-        // int oneHouseAgo = nums[1];
-        // int twoHousesAgo = nums[0];
-        int[] robbed = new int[nums.length];
-        robbed[0] = nums[0];
-        robbed[1] = Math.max(nums[0], nums[1]);
+        
+        // int[] robbed = new int[nums.length];
+        // robbed[0] = nums[0];
+        // robbed[1] = Math.max(nums[0], nums[1]);
 
-        for(int i = 2; i < robbed.length; i++ ){
-            robbed[i] = Math.max(nums[i] + robbed[i-2], robbed[i-1]);
-            // twoHousesAgo = oneHouseAgo;
-            // oneHouseAgo = maxRobbed;
+        int oneHouseAgo = Math.max(nums[0], nums[1]);
+        int twoHousesAgo = nums[0];
+        
+        for(int i = 2; i < nums.length; i++ ){
+            // robbed[i] = Math.max(nums[i] + robbed[i-2], robbed[i-1]);
+            int maxRobbed = Math.max(nums[i] + twoHousesAgo, oneHouseAgo);
+            twoHousesAgo = oneHouseAgo;
+            oneHouseAgo = maxRobbed;
         }
         // System.out.println(Arrays.toString(robbed));
-        return Math.max(robbed[robbed.length-1], robbed[robbed.length-2]);
+        // return Math.max(robbed[robbed.length-1], robbed[robbed.length-2]);
+        return oneHouseAgo;
     }
 }
