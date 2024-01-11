d6d1d30da1cb64ba4890b103ac396f6e6360216a
Abasifreke James <jamesfreky@gmail.com>
2024-01-11 11:42:38 -0500

    Time: 16 ms (72.63%) | Memory: 45.4 MB (6.54%) - LeetSync

--- 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.java
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.java
@@ -1,41 +1,45 @@
 class Solution {
     public int removeStones(int[][] stones) {
-        if (stones.length ==0 || stones[0].length ==0) return 0;
         Map<Integer, List<int[]>> mapX = new HashMap<>();
         Map<Integer, List<int[]>> mapY = new HashMap<>();
-        
-        // building map of all stone on same row/col
-        for(int[] stone : stones) {
+
+        for(int[] stone: stones){
             List<int[]> xList = mapX.getOrDefault(stone[0], new ArrayList<>());
             xList.add(stone);
             mapX.put(stone[0], xList);
-            
+
             List<int[]> yList = mapY.getOrDefault(stone[1], new ArrayList<>());
             yList.add(stone);
             mapY.put(stone[1], yList);
         }
-        
+
         Set<int[]> visited = new HashSet<>();
-        
-        int numComp = 0;
-        for(int[] stone : stones) {
-            if(!visited.contains(stone)) {
-                numComp++;
-                dfs(mapX,mapY, stone, visited);
+
+        int numOfIslands = 0;
+
+        for(int[] stone: stones){
+            if(!visited.contains(stone)){
+                numOfIslands++;
+                dfs(mapX, mapY, stone, visited);
             }
         }
-        return stones.length - numComp;
+
+        return stones.length - numOfIslands;
     }
-    
-    private void dfs(Map<Integer, List<int[]>> mapX,
-                     Map<Integer, List<int[]>> mapY,
-                     int[] stone,
-                     Set<int[]> visited) {
+
+    private void dfs(Map<Integer, List<int[]>> mapX,  Map<Integer, List<int[]>> mapY, int[] stone, Set<int[]> visited){
+        if(visited.contains(stone)){
+            return;
+        }
+
         visited.add(stone);
-        for(int[] s : mapX.get(stone[0])) 
-            if(!visited.contains(s)) dfs(mapX, mapY, s, visited);
-        
-        for(int[] s : mapY.get(stone[1])) 
-            if(!visited.contains(s)) dfs(mapX, mapY, s, visited);
+
+        for(int[] sameRowStone: mapX.get(stone[0])){
+            dfs(mapX, mapY, sameRowStone, visited);
+        }
+
+        for(int[] sameColStone: mapY.get(stone[1])){
+            dfs(mapX, mapY, sameColStone, visited);
+        }
     }
 }
