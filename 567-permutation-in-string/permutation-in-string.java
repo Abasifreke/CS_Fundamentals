80b4e272afadd7cb2e03c601123dd632e0f77ccd
Abasifreke James <jamesfreky@gmail.com>
2024-01-16 11:29:24 -0500

    Time: 9 ms (55.53%) | Memory: 42.2 MB (68.20%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -8,20 +8,30 @@ public class Solution {
             s1map[s1.charAt(i) - 'a']++;
             s2map[s2.charAt(i) - 'a']++;
         }
-        for (int i = 0; i < s2.length() - s1.length(); i++) {
-            if (matches(s1map, s2map))
-                return true;
-            s2map[s2.charAt(i + s1.length()) - 'a']++;
-            s2map[s2.charAt(i) - 'a']--;
-        }
-        return matches(s1map, s2map);
-    }
-    
-    public boolean matches(int[] s1map, int[] s2map) {
+
+        int count = 0;
         for (int i = 0; i < 26; i++) {
-            if (s1map[i] != s2map[i])
-                return false;
+            if (s1map[i] == s2map[i])
+                count++;
         }
-        return true;
+
+        for (int i = 0; i < s2.length() - s1.length(); i++) {
+            int r = s2.charAt(i + s1.length()) - 'a', l = s2.charAt(i) - 'a';
+            if (count == 26)
+                return true;
+            s2map[r]++;
+            if (s2map[r] == s1map[r]) {
+                count++;
+            } else if (s2map[r] == s1map[r] + 1) {
+                count--;
+            }
+            s2map[l]--;
+            if (s2map[l] == s1map[l]) {
+                count++;
+            } else if (s2map[l] == s1map[l] - 1) {
+                count--;
+            }
+        }
+        return count == 26;
     }
 }
