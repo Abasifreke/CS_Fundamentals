3eeb2913a59ab82681ed2431f5ddd5e03a238f14
Abasifreke James <jamesfreky@gmail.com>
2024-01-16 11:33:56 -0500

    Time: 8 ms (63.48%) | Memory: 42.8 MB (47.44%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -2,12 +2,12 @@ class Solution {
     public boolean checkInclusion(String s1, String s2) {
         if(s2.length() < s1.length()) return false;
 
-        Map<Character, Integer> s1Map = new HashMap<>();
-        Map<Character, Integer> s2Map = new HashMap<>();
+        int[] s1Map = new int[26];
+        int[] s2Map = new int[26];
+
         int need = 0, have = 0;
         for(char c : s1.toCharArray()){
-            s1Map.putIfAbsent(c, 0);
-            s1Map.put(c, s1Map.get(c) + 1);
+            s1Map[c - 'a']++;
             need++;
         }
 
@@ -15,11 +15,10 @@ class Solution {
 
         for(; right < s2.length(); ){
             char cRight = s2.charAt(right);
-            if(s1Map.containsKey(cRight)){
-                s2Map.putIfAbsent(cRight, 0);
-                s2Map.put(cRight, s2Map.get(cRight) + 1);
+            if(s1Map[cRight - 'a'] > 0){
+                s2Map[cRight - 'a']++;
 
-                if(s2Map.get(cRight) <= s1Map.get(cRight)){
+                if(s2Map[cRight- 'a'] <= s1Map[cRight - 'a']){
                     have++;
                 }
             }
@@ -30,10 +29,10 @@ class Solution {
                 if(have == need)return true;
 
                 char cLeft = s2.charAt(left++);
-                if(s2Map.containsKey(cLeft) && s2Map.get(cLeft) > 0){
-                    s2Map.put(cLeft, s2Map.get(cLeft) - 1);
+                if(s2Map[cLeft - 'a'] > 0){
+                    s2Map[cLeft - 'a']--;
 
-                    if(s2Map.get(cLeft) < s1Map.get(cLeft)){
+                    if(s2Map[cLeft - 'a'] < s1Map[cLeft - 'a']){
                         have--;
                     }   
                 }
