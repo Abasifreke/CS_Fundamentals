7f505b27f35029378c0aaa04e575123e0dadf4a3
Abasifreke James <jamesfreky@gmail.com>
2024-01-16 11:29:52 -0500

    Time: 37 ms (28.14%) | Memory: 43.1 MB (38.10%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,37 +1,45 @@
-public class Solution {
+class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        if (s1.length() > s2.length())
-            return false;
-        int[] s1map = new int[26];
-        int[] s2map = new int[26];
-        for (int i = 0; i < s1.length(); i++) {
-            s1map[s1.charAt(i) - 'a']++;
-            s2map[s2.charAt(i) - 'a']++;
+        if(s2.length() < s1.length()) return false;
+
+        Map<Character, Integer> s1Map = new HashMap<>();
+        Map<Character, Integer> s2Map = new HashMap<>();
+        int need = 0, have = 0;
+        for(char c : s1.toCharArray()){
+            s1Map.putIfAbsent(c, 0);
+            s1Map.put(c, s1Map.get(c) + 1);
+            need++;
         }
 
-        int count = 0;
-        for (int i = 0; i < 26; i++) {
-            if (s1map[i] == s2map[i])
-                count++;
+        int left = 0, right = 0;
+
+        for(; right < s2.length(); ){
+            char cRight = s2.charAt(right);
+            if(s1Map.containsKey(cRight)){
+                s2Map.putIfAbsent(cRight, 0);
+                s2Map.put(cRight, s2Map.get(cRight) + 1);
+
+                if(s2Map.get(cRight) <= s1Map.get(cRight)){
+                    have++;
+                }
+            }
+
+            right++;
+
+            if((right-left) == s1.length()){
+                if(have == need)return true;
+
+                char cLeft = s2.charAt(left++);
+                if(s2Map.containsKey(cLeft) && s2Map.get(cLeft) > 0){
+                    s2Map.put(cLeft, s2Map.get(cLeft) - 1);
+
+                    if(s2Map.get(cLeft) < s1Map.get(cLeft)){
+                        have--;
+                    }   
+                }
+            }
         }
 
-        for (int i = 0; i < s2.length() - s1.length(); i++) {
-            int r = s2.charAt(i + s1.length()) - 'a', l = s2.charAt(i) - 'a';
-            if (count == 26)
-                return true;
-            s2map[r]++;
-            if (s2map[r] == s1map[r]) {
-                count++;
-            } else if (s2map[r] == s1map[r] + 1) {
-                count--;
-            }
-            s2map[l]--;
-            if (s2map[l] == s1map[l]) {
-                count++;
-            } else if (s2map[l] == s1map[l] - 1) {
-                count--;
-            }
-        }
-        return count == 26;
+        return false;
     }
 }
