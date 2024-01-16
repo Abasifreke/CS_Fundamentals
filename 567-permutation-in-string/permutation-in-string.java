5c02307905d11203ff5a05b431b157ddad5770ba
Abasifreke James <jamesfreky@gmail.com>
2024-01-16 11:27:36 -0500

    Time: 32 ms (30.81%) | Memory: 43.2 MB (37.07%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,28 +1,45 @@
-public class Solution {
+class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        if (s1.length() > s2.length())
-            return false;
-        HashMap<Character, Integer> s1map = new HashMap<>();
+        if(s2.length() < s1.length()) return false;
 
-        for (int i = 0; i < s1.length(); i++)
-            s1map.put(s1.charAt(i), s1map.getOrDefault(s1.charAt(i), 0) + 1);
+        Map<Character, Integer> s1Map = new HashMap<>();
+        Map<Character, Integer> s2Map = new HashMap<>();
+        int need = 0, have = 0;
+        for(char c : s1.toCharArray()){
+            s1Map.putIfAbsent(c, 0);
+            s1Map.put(c, s1Map.get(c) + 1);
+            need++;
+        }
 
-        for (int i = 0; i <= s2.length() - s1.length(); i++) {
-            HashMap<Character, Integer> s2map = new HashMap<>();
-            for (int j = 0; j < s1.length(); j++) {
-                s2map.put(s2.charAt(i + j), s2map.getOrDefault(s2.charAt(i + j), 0) + 1);
+        int left = 0, right = 0;
+
+        for(; right < s2.length(); ){
+            char cRight = s2.charAt(right);
+            if(s1Map.containsKey(cRight)){
+                s2Map.putIfAbsent(cRight, 0);
+                s2Map.put(cRight, s2Map.get(cRight) + 1);
+
+                if(s2Map.get(cRight) <= s1Map.get(cRight)){
+                    have++;
+                }
             }
-            if (matches(s1map, s2map))
-                return true;
-        }
-        return false;
-    }
 
-    public boolean matches(HashMap<Character, Integer> s1map, HashMap<Character, Integer> s2map) {
-        for (char key : s1map.keySet()) {
-            if (s1map.get(key) - s2map.getOrDefault(key, -1) != 0)
-                return false;
+            right++;
+
+            if((right-left) == s1.length()){
+                if(have == need)return true;
+
+                char cLeft = s2.charAt(left++);
+                if(s2Map.containsKey(cLeft) && s2Map.get(cLeft) > 0){
+                    s2Map.put(cLeft, s2Map.get(cLeft) - 1);
+
+                    if(s2Map.get(cLeft) < s1Map.get(cLeft)){
+                        have--;
+                    }   
+                }
+            }
         }
-        return true;
+
+        return false;
     }
 }
