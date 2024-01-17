d0cf7cf915989a0cfd7534b69e0cd71a78061cbd
Abasifreke James <jamesfreky@gmail.com>
2024-01-17 14:23:48 -0500

    Time: 0 ms (100.00%) | Memory: 41.3 MB (6.53%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -13,14 +13,15 @@ class Solution {
 
         if(n == 1) return nums[0];
         
-        int[] dp = new int[n];
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0], nums[1]);
+        int twoNaway = nums[0];
+        int oneNaway = Math.max(nums[0], nums[1]);
 
         for(int i = 2; i < n; i++){
-            dp[i] = Math.max(dp[i-1], nums[i] + dp[i-2]);
+            int iThAnswer = Math.max(oneNaway, nums[i] + twoNaway);
+            twoNaway = oneNaway;
+            oneNaway = iThAnswer;
         }
 
-        return Math.max(dp[n-1], dp[n-2]);
+        return oneNaway;
     }
 }
