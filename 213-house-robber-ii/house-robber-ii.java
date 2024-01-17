1a88aedc1d148386e438afa0bcb4346a345f95e8
Abasifreke James <jamesfreky@gmail.com>
2024-01-17 14:51:53 -0500

    Time: 0 ms (100.00%) | Memory: 40.9 MB (37.62%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -2,7 +2,7 @@ class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
         if(n == 1) return nums[0];
-        if(n == 2) return Math.max(nums[0], nums[1]);
+        // if(n == 2) return Math.max(nums[0], nums[1]);
 
         return Math.max(rob1(nums, 0, n-1), rob1(nums, 1, n));
     }
@@ -12,10 +12,10 @@ class Solution {
 
         if(n == 1) return nums[start];
         
-        int twoNaway = nums[start];
-        int oneNaway = Math.max(nums[start], nums[start + 1]);
+        int twoNaway = 0;
+        int oneNaway = 0;
 
-        for(int i = start + 2; i < end; i++){
+        for(int i = start; i < end; i++){
             int iThAnswer = Math.max(oneNaway, nums[i] + twoNaway);
             twoNaway = oneNaway;
             oneNaway = iThAnswer;
