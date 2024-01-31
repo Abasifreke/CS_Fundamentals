d43af858052eb630fe8202801e08a7b5afe0fe8d
Abasifreke James <jamesfreky@gmail.com>
2024-01-31 10:19:32 -0500

    Time: 6 ms (78.07%) | Memory: 44 MB (78.89%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
@@ -1,37 +1,54 @@
 class Solution {
+    private static final Map<String, BiFunction<Integer, Integer, Integer>> OPERATIONS = new HashMap<>();
+
+    static {
+        OPERATIONS.put("+", (a, b) -> b + a);
+        OPERATIONS.put("-", (a, b) -> b - a);
+        OPERATIONS.put("*", (a, b) -> b * a);
+        OPERATIONS.put("/", (a, b) -> b / a);
+    }
+
     public int evalRPN(String[] tokens) {
         Stack<Integer> operands = new Stack<>();
 
         for(String token: tokens){
-            try{
-                int tokenAsNum = Integer.parseInt(token);
-                operands.push(tokenAsNum);
-            }
-            catch(NumberFormatException nfException){
-                Integer opResult = performOperation(token, operands.pop(), operands.pop());
-                if(opResult == null) return -1;
+            // try{
+            //     int tokenAsNum = Integer.parseInt(token);
+            //     operands.push(tokenAsNum);
+            // }
+            // catch(NumberFormatException nfException){
+            //     Integer opResult = performOperation(token, operands.pop(), operands.pop());
+            //     if(opResult == null) return -1;
 
-                operands.push(opResult);
+            //     operands.push(opResult);
+            // }
+
+            if(!"+-*/".contains(token)){
+                operands.push(Integer.parseInt(token));
+            }else{
+                operands.push(OPERATIONS.get(token).apply(operands.pop(), operands.pop()));
             }
         }
 
         return operands.pop();
     }
 
-    private Integer performOperation(String token, int operand1, int operand2){
-        if(token.equals("+")){
-            return operand2 + operand1;
-        }
-        else if(token.equals("-")){
-            return operand2 - operand1;
-        }
-        else if(token.equals("*")){
-            return operand2 * operand1;
-        }
-        else if(token.equals("/")){
-            return operand2 / operand1;
-        }else{
-            return null;
-        }
-    }
+    
+
+    // private Integer performOperation(String token, int operand1, int operand2){
+    //     if(token.equals("+")){
+    //         return operand2 + operand1;
+    //     }
+    //     else if(token.equals("-")){
+    //         return operand2 - operand1;
+    //     }
+    //     else if(token.equals("*")){
+    //         return operand2 * operand1;
+    //     }
+    //     else if(token.equals("/")){
+    //         return operand2 / operand1;
+    //     }else{
+    //         return null;
+    //     }
+    // }
 }
