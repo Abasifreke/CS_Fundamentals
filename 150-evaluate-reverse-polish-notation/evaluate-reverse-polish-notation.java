183841264b983ba43891867d320d4aa2918e81a0
Abasifreke James <jamesfreky@gmail.com>
2024-01-31 10:41:40 -0500

    Time: 6 ms (78.07%) | Memory: 44.6 MB (36.31%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
@@ -1,6 +1,5 @@
 class Solution {
     private static final Map<String, BiFunction<Integer, Integer, Integer>> OPERATIONS = new HashMap<>();
-
     static {
         OPERATIONS.put("+", (a, b) -> b + a);
         OPERATIONS.put("-", (a, b) -> b - a);
@@ -12,17 +11,6 @@ class Solution {
         Stack<Integer> operands = new Stack<>();
 
         for(String token: tokens){
-            // try{
-            //     int tokenAsNum = Integer.parseInt(token);
-            //     operands.push(tokenAsNum);
-            // }
-            // catch(NumberFormatException nfException){
-            //     Integer opResult = performOperation(token, operands.pop(), operands.pop());
-            //     if(opResult == null) return -1;
-
-            //     operands.push(opResult);
-            // }
-
             if(!"+-*/".contains(token)){
                 operands.push(Integer.parseInt(token));
             }else{
@@ -32,23 +20,4 @@ class Solution {
 
         return operands.pop();
     }
-
-    
-
-    // private Integer performOperation(String token, int operand1, int operand2){
-    //     if(token.equals("+")){
-    //         return operand2 + operand1;
-    //     }
-    //     else if(token.equals("-")){
-    //         return operand2 - operand1;
-    //     }
-    //     else if(token.equals("*")){
-    //         return operand2 * operand1;
-    //     }
-    //     else if(token.equals("/")){
-    //         return operand2 / operand1;
-    //     }else{
-    //         return null;
-    //     }
-    // }
 }
