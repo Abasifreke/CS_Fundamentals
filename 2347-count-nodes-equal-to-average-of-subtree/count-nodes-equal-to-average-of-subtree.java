b23f811a634d358b37bd92a884c169f11fdaeff1
Abasifreke James <jamesfreky@gmail.com>
2024-02-01 12:38:14 -0500

    Time: 1 ms (57.55%) | Memory: 43.8 MB (59.15%) - LeetSync

--- 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.java
+++ 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.java
@@ -1,41 +1,29 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-
 class Solution {
+    int count = 0;
+    
+    Pair<Integer, Integer> postOrder(TreeNode root) {
+        if (root == null) {
+            return new Pair(0, 0);
+        }
+        
+        // First iterate over left and right subtrees.
+        Pair<Integer, Integer> left = postOrder(root.left);
+        Pair<Integer, Integer> right = postOrder(root.right);
+        
+        int nodeSum = left.getKey() + right.getKey() + root.val;
+        int nodeCount = left.getValue() + right.getValue() + 1;
+
+        // Check if the average of the subtree is equal to the node value.
+        if (root.val == nodeSum / (nodeCount)) {
+            count++;
+        }
+        
+        // Return the sum of nodes and the count in the subtree.
+        return new Pair(nodeSum, nodeCount);
+    }
+    
     public int averageOfSubtree(TreeNode root) {
-        int[] counter = {0};
-        postOrder(root, counter);
-        return counter[0];
+        postOrder(root);
+        return count;
     }
-
-    private int[] postOrder(TreeNode node, int[] counter){
-        if(node == null) return new int[]{0, 0};
-
-
-        int[] left = postOrder(node.left, counter);
-        int[] right = postOrder(node.right, counter);
-
-        int count = left[0] + right[0] + 1;
-        int sum = left[1] + right[1] + node.val;
-        int[] result = new int[]{count, sum};
-
-        if(node.val == sum/count) counter[0]++;
-
-        return result;
-    }
-
-
 }
