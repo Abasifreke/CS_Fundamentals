f67b60dad575e24c2fc83c3071a34aaa6e7c412d
Abasifreke James <jamesfreky@gmail.com>
2024-02-01 13:48:01 -0500

    Time: 29 ms (99.88%) | Memory: 44.8 MB (69.96%) - LeetSync

--- 976-minimum-area-rectangle/minimum-area-rectangle.java
+++ 976-minimum-area-rectangle/minimum-area-rectangle.java
@@ -1,43 +1,47 @@
 class Solution {
-    public int minAreaRect(int[][] points) {
-        Map<Integer, Set<Integer>> mapX = new HashMap<>();
-        for (int[] p : points) {
-            mapX.computeIfAbsent(p[0], k -> new HashSet<>()).add(p[1]);
+    private int getSize(int[][] points,int index) {
+        Set<Integer> set = new HashSet<>();
+        for(int[]point:points){
+            set.add(point[index]);
         }
-        int ans = Integer.MAX_VALUE;
-        for (int[] p1 : points) {
-            for (int[] p2 : points) {
-                if (p1[0] == p2[0] || p1[1] == p2[1]) continue;
-                int x1 = p1[0], y1 = p1[1], x2 = p2[0], y2 = p2[1];
-                int area =  Math.abs(x1-x2) * Math.abs(y1-y2);
-                if (area < ans && mapX.get(x1).contains(y2) && mapX.get(x2).contains(y1)) {
-                    ans = area;
+        return set.size();
+    }
+    private void generateMap(Map<Integer,List<Integer>> map, int[][]points, int index){
+        for(int[]point:points){
+            int key = point[index];
+            int value = point[1-index];
+            map.putIfAbsent(key, new ArrayList<>());
+            map.get(key).add(value);
+        }
+    }
+    public int minAreaRect(int[][] points) {
+        int rowSize = getSize(points,0);
+        int colSize = getSize(points,1);
+        TreeMap<Integer,List<Integer>> map = new TreeMap<>();
+        if(rowSize > colSize) {
+            generateMap(map,points,0);
+        } else {
+            generateMap(map,points,1);
+        }
+        Map<Pair<Integer,Integer>,Integer> lastIndex = new HashMap<>();
+        int min = Integer.MAX_VALUE;
+        for(Integer key: map.keySet()){
+            List<Integer> list = map.get(key);
+            Collections.sort(list);
+            int currentY = key;
+            for(int i=0;i<list.size();i++){
+                 int x1 = list.get(i);
+                for(int j=0;j<i;j++){           
+                    int x2 = list.get(j);
+                    Pair lastIndexKey = new Pair(x1,x2);
+                    if(lastIndex.containsKey(lastIndexKey)){
+                        min = Math.min(min, (currentY-lastIndex.get(lastIndexKey))*(x1-x2));
+                    }
+                    lastIndex.put(lastIndexKey,currentY);
                 }
             }
         }
-        return ans == Integer.MAX_VALUE ? 0 : ans;
+        return min==Integer.MAX_VALUE?0:min;
+        
     }
 }
-
-/*
-1: [1, 3]
-2: 2
-3: [1, 3]
-
-x: y
-1: [1, 3]
-Side: p1, p2, len => search for Side with same p1.y and p2.y
-3: [1, 3]
-4: [1, 3]
-
-(1,1) (1,3)
-(3,1) (3,3)
-(4,1) (4,3)
-
-y: x 
-1: [1, 3, 4]
-3: [1, 3, 4]
-
-(1,1) (3,1) (4,1)
-(1,3) (3,3) (4,3)
-*/
