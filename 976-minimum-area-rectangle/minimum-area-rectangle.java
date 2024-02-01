65022670d62d69208c767a000cf579fdeba9f6ac
Abasifreke James <jamesfreky@gmail.com>
2024-02-01 13:40:01 -0500

    Time: 1109 ms (6.23%) | Memory: 45.4 MB (16.48%) - LeetSync

--- 976-minimum-area-rectangle/minimum-area-rectangle.java
+++ 976-minimum-area-rectangle/minimum-area-rectangle.java
@@ -1,24 +1,25 @@
 class Solution {
-    public int minAreaRect(int[][] points) {
-        int minArea = Integer.MAX_VALUE;
-        int n = points.length;
-
-        Arrays.sort(points, (a, b) -> a[0] - b[0]);
-        HashSet<String> pointSet = new HashSet<>();
-        for(int[] point: points){
-            pointSet.add(encode(point[0], point[1]));
+   
+    public static int minAreaRect(int[][] points) {
+        Set<String> pointSet = new HashSet<>();
+        for (int[] point : points) {
+            String encodedPoint = encode(point[0], point[1]);
+            pointSet.add(encodedPoint);
         }
 
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                if(i != j){
-                    int[] point1 = points[i];
-                    int[] point2 = points[j];
-                    
-                    if(point1[0] == point2[0] || point1[1] == point2[1]) continue;
-                    
-                    if(pointSet.contains(encode(point1[0], point2[1])) && pointSet.contains(encode(point2[0], point1[1]))){
-                        minArea = Math.min(minArea, area(point1, point2));
+        int minArea = Integer.MAX_VALUE;
+        for (int i = 0; i < points.length; ++i) {
+            for (int j = 0; j < points.length; ++j) {
+                if (i != j) {
+                    int[] p1 = points[i];
+                    int[] p2 = points[j];
+                    if (p1[0] != p2[0] && p1[1] != p2[1]) {
+                        String p3 = encode(p1[0], p2[1]);
+                        String p4 = encode(p2[0], p1[1]);
+                        if (pointSet.contains(p3) && pointSet.contains(p4)) {
+                            int area = Math.abs(p1[0] - p2[0]) * Math.abs(p1[1] - p2[1]);
+                            minArea = Math.min(minArea, area);
+                        }
                     }
                 }
             }
@@ -27,11 +28,8 @@ class Solution {
         return minArea == Integer.MAX_VALUE ? 0 : minArea;
     }
 
-    private String encode(int x, int y){
-        return x + "|" + y;
+    private static String encode(int x, int y) {
+        return x + "," + y;
     }
 
-    private int area(int[] point1, int[] point2){
-        return Math.abs(point1[0]-point2[0]) * Math.abs(point1[1] - point2[1]); 
-    }
 }
