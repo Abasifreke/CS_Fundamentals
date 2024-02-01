0dc9a83fea17acec8170f9a0e890a0a79602d4b5
Abasifreke James <jamesfreky@gmail.com>
2024-02-01 14:11:53 -0500

    Time: 368 ms (42.12%) | Memory: 45 MB (50.18%) - LeetSync

--- 976-minimum-area-rectangle/minimum-area-rectangle.java
+++ 976-minimum-area-rectangle/minimum-area-rectangle.java
@@ -9,9 +9,9 @@ class Solution {
             for (int[] p2 : points) {
                 if (p1[0] == p2[0] || p1[1] == p2[1]) continue;
                 int x1 = p1[0], y1 = p1[1], x2 = p2[0], y2 = p2[1];
-                int area =  Math.abs(x1-x2) * Math.abs(y1-y2);
-                if (area < ans && mapX.get(x1).contains(y2) && mapX.get(x2).contains(y1)) {
-                    ans = area;
+                if (mapX.get(x1).contains(y2) && mapX.get(x2).contains(y1)) {
+                    int area =  Math.abs(x1-x2) * Math.abs(y1-y2);
+                    ans = Math.min(ans, area);
                 }
             }
         }
