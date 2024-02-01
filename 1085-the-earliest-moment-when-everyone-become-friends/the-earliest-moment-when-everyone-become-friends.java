f7577c669be109ad9f08bc1b1530c4d4c0d27b26
Abasifreke James <jamesfreky@gmail.com>
2024-02-01 12:04:38 -0500

    Time: 7 ms (38.54%) | Memory: 45 MB (6.72%) - LeetSync

--- 1085-the-earliest-moment-when-everyone-become-friends/the-earliest-moment-when-everyone-become-friends.java
+++ 1085-the-earliest-moment-when-everyone-become-friends/the-earliest-moment-when-everyone-become-friends.java
@@ -1,53 +1,85 @@
 class Solution {
     public int earliestAcq(int[][] logs, int n) {
-        // sort logs if needed.
-        Arrays.sort(logs,(a, b) -> a[0] - b[0]);
-        Map<Integer, List<Integer>> graph = new HashMap<>();
-        HashSet<Integer> unconnected = new HashSet<>();
 
-        for(int i = 0; i < n; i++){
-            unconnected.add(i);
-        }
+        // First, we need to sort the events in chronological order.
+        Arrays.sort(logs, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] log1, int[] log2) {
+                Integer tsp1 = new Integer(log1[0]);
+                Integer tsp2 = new Integer(log2[0]);
+                return tsp1.compareTo(tsp2);
+            }
+        });
 
-        for(int[] log: logs){
-            Integer a = log[1];
-            Integer b = log[2];
+        // Initially, we treat each individual as a separate group.
+        int groupCount = n;
+        UnionFind uf = new UnionFind(n);
 
-            unconnected.remove(a);
-            unconnected.remove(b);
+        for (int[] log : logs) {
+            int timestamp = log[0], friendA = log[1], friendB = log[2];
 
-            graph.putIfAbsent(a, new ArrayList<>());
-            graph.get(a).add(b);
+            // We merge the groups along the way.
+            if (uf.union(friendA, friendB)) {
+                groupCount -= 1;
+            }
 
-            graph.putIfAbsent(b, new ArrayList<>());
-            graph.get(b).add(a);
-
-
-            if(unconnected.isEmpty()){
-                List<Integer> reachableNodes = new ArrayList<>();
-                reachableFromNode(graph, 0, new HashSet<Integer>(), reachableNodes);
-
-                if(reachableNodes.size() == n){
-                    return log[0];
-                }
+            // The moment when all individuals are connected to each other.
+            if (groupCount == 1) {
+                return timestamp;
             }
         }
 
-        // if(!unconnected.isEmpty()){
-            return -1;
-        // }
+        // There are still more than one groups left,
+        //  i.e. not everyone is connected.
+        return -1;
+    }
+}
+
+class UnionFind {
+    private int[] group;
+    private int[] rank;
+
+    public UnionFind(int size) {
+        this.group = new int[size];
+        this.rank = new int[size];
+        for (int person = 0; person < size; ++person) {
+            this.group[person] = person;
+            this.rank[person] = 0;
+        }
     }
 
-    private void reachableFromNode(Map<Integer, List<Integer>> graph, int i, HashSet<Integer> visited, List<Integer> reachableNodes){
-        if(visited.contains(i)){
-            return;
+    /** Return the id of group that the person belongs to. */
+    public int find(int person) {
+        if (this.group[person] != person)
+            this.group[person] = this.find(this.group[person]);
+        return this.group[person];
+    }
+
+    /**
+     * If it is necessary to merge the two groups that x, y belong to.
+     * @return true: if the groups are merged.
+     */
+    public boolean union(int a, int b) {
+        int groupA = this.find(a);
+        int groupB = this.find(b);
+        boolean isMerged = false;
+
+        // The two people share the same group.
+        if (groupA == groupB)
+            return isMerged;
+
+        // Otherwise, merge the two groups.
+        isMerged = true;
+        // Merge the lower-rank group into the higher-rank group.
+        if (this.rank[groupA] > this.rank[groupB]) {
+            this.group[groupB] = groupA;
+        } else if (this.rank[groupA] < this.rank[groupB]) {
+            this.group[groupA] = groupB;
+        } else {
+            this.group[groupA] = groupB;
+            this.rank[groupB] += 1;
         }
 
-        visited.add(i);
-        reachableNodes.add(i);
-
-        for(int node: graph.get(i)){
-            reachableFromNode(graph, node, visited, reachableNodes);
-        }
+        return isMerged;
     }
 }
