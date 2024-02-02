c209c9f4f40c8c1faed9febda4b30e4261dcefe7
Abasifreke James <jamesfreky@gmail.com>
2024-02-02 15:39:48 -0500

    Time: 242 ms (5.06%) | Memory: 46.9 MB (5.59%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,45 +1,60 @@
 class Solution {
+    public boolean canFinish(int n, int[][] prerequisites) {
+        /*
+            0.... numCourses
 
-   
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
-        Map<Integer, List<Integer>> coursePreRequ = new HashMap<>();
+            courses without prereq = q = {}
+            course to prereq() = <int , hashset<int>>
 
-        for(int[] prereq: prerequisites){
-            int course = prereq[0];
-            int pre = prereq[1];
-            coursePreRequ.computeIfAbsent(course, k-> new ArrayList<>()).add(pre);
+            add all courses without prereque
+            while(!q.isEmpty){
+               remove current course from all lists that have it. 
+                 if list becomes emput, add to queue.
+                 add to visited set. 
+
+                 if(visit contains a current course, means there's a ycle return false.')
+            }
+        */
+
+        Map<Integer, HashSet<Integer>> courseToPre = new HashMap<>();
+        
+        for(int[] combo: prerequisites){
+            int course = combo[0];
+            int pre = combo[1];
+
+            courseToPre.computeIfAbsent(course, k-> new HashSet<>()).add(pre);
         }
 
+        LinkedList<Integer> q = new LinkedList<>();
+        Set<Integer> visited = new HashSet<>();
+        int coursesTaken = 0;
 
-        for(int i = 0; i < numCourses; i++){
-            if(!canTakeCourse(i, coursePreRequ, new HashSet<>())){
-                return false;
+        for(int i = 0; i < n; i++){
+            if(!courseToPre.containsKey(i)){
+                q.offer(i);
             }
         }
+                System.out.println(courseToPre);
+                System.out.println(q);
 
-        return true;
-    }
+        while(!q.isEmpty()){
+            int course = q.poll();
+            System.out.println(coursesTaken);
+            if(++coursesTaken > n) return false;
 
-    public boolean canTakeCourse(int k, Map<Integer, List<Integer>> coursePreRequ, HashSet<Integer> visited){
-        if(visited.contains(k)){
-            return false;
-        }
+            for(int key: courseToPre.keySet()){
+                if(!visited.contains(key) && courseToPre.get(key).contains(course)){
+                    courseToPre.get(key).remove(course);
 
-        if(!coursePreRequ.containsKey(k)){
-            coursePreRequ.remove(k);
-            return true;
-        }
-
-        visited.add(k);
-        for(int n: coursePreRequ.get(k)){
-            if(!canTakeCourse(n, coursePreRequ, visited)){
-                return false;
+                    if(courseToPre.get(key).size() == 0){
+                        visited.add(key);
+                        q.add(key);
+                    }
+                }
             }
+
         }
 
-        visited.remove(k);
-        coursePreRequ.remove(k);
-        return true;
+        return coursesTaken == n;
     }
-       
 }
