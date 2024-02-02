10760a72ba56134e3dc5b3b5e7510c358668da11
Abasifreke James <jamesfreky@gmail.com>
2024-02-02 13:03:19 -0500

    Time: 0 ms (100.00%) | Memory: 41.4 MB (5.12%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,27 +1,19 @@
 class Solution {
     public int rob(int[] nums) {
-        
-        /*
-            [2,7,9,3,1] 
-            [1, 3, 2, 1]
-
-            //[1, max(1,2), max(i-1, val(i) + i-2),  val(i) + i-2), val(i) + i-2)]
-            [2,7,11,11,12] 
-            [1, 3, 3, 4] = 4
-        */
-        int n = nums.length;
+        int n = nums.length;;
 
         if(n == 1) return nums[0];
-        
-        int twoNaway = nums[0];
-        int oneNaway = Math.max(nums[0], nums[1]);
+        if(n == 2) return Math.max(nums[0], nums[1]);
+
+        int dp1 = nums[0];
+        int dp2 = Math.max(nums[0], nums[1]);
 
         for(int i = 2; i < n; i++){
-            int iThAnswer = Math.max(oneNaway, nums[i] + twoNaway);
-            twoNaway = oneNaway;
-            oneNaway = iThAnswer;
+            int sum = Math.max(nums[i] + dp1, dp2);
+            dp1 = dp2;
+            dp2 = sum;
         }
 
-        return oneNaway;
+        return dp2;
     }
 }
