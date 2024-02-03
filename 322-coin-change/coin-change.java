c5e4df79c649139483ff315454e8f993aff03e8c
Abasifreke James <jamesfreky@gmail.com>
2024-02-03 12:55:55 -0500

    Time: 14 ms (88.27%) | Memory: 44.5 MB (43.55%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -3,21 +3,11 @@ class Solution {
         int[] dp = new int[amount + 1];
         Arrays.fill(dp, amount + 1);
 
-        HashSet<Integer> coinSet = new HashSet<>();
-        for(int coin: coins){
-            coinSet.add(coin);
-        }
-
         dp[0] = 0;
-
         for(int i = 1; i < dp.length; i++){
-            if(coinSet.contains(i)){
-                dp[i] = 1;
-            }else{
-                for(int coin: coinSet){
-                    if(coin < i){
-                        dp[i] = Math.min(dp[i], (1 + dp[(i - coin)]));
-                    }
+            for(int coin: coins){
+                if(coin <= i){
+                    dp[i] = Math.min(dp[i], (1 + dp[(i - coin)]));
                 }
             }
         }
