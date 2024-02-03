c806510b8174779b314fc627d8e3ac3e3640364a
Abasifreke James <jamesfreky@gmail.com>
2024-02-03 15:55:58 -0500

    Time: 5 ms (72.73%) | Memory: 44.3 MB (75.24%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,56 +1,41 @@
 class Solution {
     public boolean canFinish(int n, int[][] prerequisites) {
-        /*
-            0.... numCourses
-
-            courses without prereq = q = {}
-            course to prereq() = <int , hashset<int>>
-
-            add all courses without prereque
-            while(!q.isEmpty){
-               remove current course from all lists that have it. 
-                 if list becomes emput, add to queue.
-                 add to visited set. 
-
-                 if(visit contains a current course, means there's a ycle return false.')
-            }
-        */
-
-        Map<Integer, HashSet<Integer>> courseToPre = new HashMap<>();
+        int[] inDegree = new int[n];
+        List<List<Integer>> preToCourses = new ArrayList<>();
         
+        for(int i = 0; i < n; i++){
+            preToCourses.add(new ArrayList<>());
+        }
+
         for(int[] combo: prerequisites){
             int course = combo[0];
             int pre = combo[1];
 
-            courseToPre.computeIfAbsent(course, k-> new HashSet<>()).add(pre);
+            preToCourses.get(pre).add(course);
+            inDegree[course]++;
         }
 
         LinkedList<Integer> q = new LinkedList<>();
         int coursesTaken = 0;
 
         for(int i = 0; i < n; i++){
-            if(!courseToPre.containsKey(i)){
+            if(inDegree[i] == 0){
                 q.offer(i);
             }
         }
             
         while(!q.isEmpty()){
-            int course = q.poll();
+            int pre = q.poll();
             if(++coursesTaken > n) return false;
 
-            Iterator<Map.Entry<Integer, HashSet<Integer>>> iterator = courseToPre.entrySet().iterator();
-            while(iterator.hasNext()){
-                Map.Entry<Integer, HashSet<Integer>> entry = iterator.next();
-                int key = entry.getKey();
-
-                if(entry.getValue().contains(course)){
-                    courseToPre.get(key).remove(course);
-
-                    if(courseToPre.get(key).size() == 0){
-                        q.add(key);
-                    }
+            for(int course: preToCourses.get(pre)){
+                inDegree[course]--;
+                
+                if(inDegree[course] == 0){
+                    q.add(course);
                 }
             }
+            
 
         }
 
