be7ef2d741c00d229c66b19a91a288bfcca6bcc4
Abasifreke James <jamesfreky@gmail.com>
2024-02-05 12:53:15 -0500

    Time: 5 ms (97.13%) | Memory: 46.5 MB (5.74%) - LeetSync

--- 1628-count-submatrices-with-all-ones/count-submatrices-with-all-ones.java
+++ 1628-count-submatrices-with-all-ones/count-submatrices-with-all-ones.java
@@ -1,28 +1,14 @@
 class Solution {
     public int numSubmat(int[][] mat) {
         int m = mat.length, n = mat[0].length;
-        int[][] precompute = new int[m][n];
-
-        // precompute
-         for(int i = 0; i < m; i++){
-             
-            int count = 0;
-            for(int j = n-1; j >= 0; j--){
-                if(mat[i][j] == 1){
-                    precompute[i][j] = ++count;
-                }else {
-                    count = 0;
-                }
-            }
-         }
+        int[] height = new int[n];
 
         int acc = 0;
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
-                int min = Integer.MAX_VALUE;
-
-                for(int k = i; k < m; k++){
-                    min = Math.min(min, precompute[k][j]);
+                height[j] = mat[i][j] == 0 ? 0 : height[j] + 1;   //  height of histogram;
+                for (int k = j, min = height[j]; k >= 0 && min > 0; k--) {
+                    min = Math.min(min, height[k]);
                     acc += min;
                 }
             }
