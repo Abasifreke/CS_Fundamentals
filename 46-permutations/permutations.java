3aa1e2d0d4da645683ac09c4bef34a6a6cde9aa0
Abasifreke James <jamesfreky@gmail.com>
2024-02-05 20:00:37 -0500

    Time: 1 ms (95.93%) | Memory: 44.5 MB (45.73%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,21 +1,24 @@
 class Solution {
-    public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
-        backtrack(new ArrayList<>(), ans, nums);
-        return ans;
-    }
     
-    public void backtrack(List<Integer> curr, List<List<Integer>> ans, int[] nums) {
-        if (curr.size() == nums.length) {
-            ans.add(new ArrayList<>(curr));
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        permRecur(nums, result, new ArrayList<>());
+
+        return result;
+    }
+
+    private void permRecur(int[] nums, List<List<Integer>> result, List<Integer> accumulator){
+        
+        if(accumulator.size() == nums.length){
+            result.add(new ArrayList<>(accumulator));
             return;
         }
-        
-        for (int num: nums) {
-            if (!curr.contains(num)) {
-                curr.add(num);
-                backtrack(curr, ans, nums);
-                curr.remove(curr.size() - 1);
+
+        for(int choice: nums){
+            if(!accumulator.contains(choice)){
+                accumulator.add(choice);
+                permRecur(nums, result, accumulator);
+                accumulator.removeLast();
             }
         }
     }
