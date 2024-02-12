6caff60fe5350110201b9db7532bafe6e8f59f9c
Abasifreke James <jamesfreky@gmail.com>
2024-02-12 13:03:17 -0500

    Time: 2 ms (57.08%) | Memory: 44.7 MB (98.48%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,47 +1,50 @@
-class UnionFind {
-    int[] parent;
-    int[] rank;
-    public UnionFind(int size) {
-        parent = new int[size];
-        for (int i = 0; i < size; i++)
-            parent[i] = i;
-        rank = new int[size];
-    }
-    public int find(int x) {
-        if (parent[x] != x)
-            parent[x] = find(parent[x]);
-        return parent[x];
-    }
-    public int union_set(int x, int y) {
-        int xset = find(x), yset = find(y);
-        if (xset == yset) {
-            return 0;
-        } else if (rank[xset] < rank[yset]) {
-            parent[xset] = yset;
-        } else if (rank[xset] > rank[yset]) {
-            parent[yset] = xset;
-        } else {
-            parent[yset] = xset;
-            rank[xset]++;
-        }
-        return 1;
-    }
-}
-
 class Solution {
     public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length;
-        UnionFind dsu = new UnionFind(n);
-        int numberOfComponents = n;
+        int[] unionSet = new int[n];
+        int[] rank = new int[n];
 
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (isConnected[i][j] == 1) {
-                    numberOfComponents -= dsu.union_set(i, j);
+        for(int i = 0; i < unionSet.length; i++){
+            unionSet[i] = i;
+            rank[i] = 1;
+        }
+
+        int result = n;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(i != j && isConnected[i][j] == 1){
+                    result -= union(unionSet, rank, i, j);
                 }
             }
         }
+   
+        return result;
+    }
 
-        return numberOfComponents;
+    private int union(int[] unionSet, int[] rank, int nodeA, int nodeB){
+        int rootOfA = find(unionSet, nodeA);
+        int rootOfB = find(unionSet, nodeB);
+
+        if(rootOfA != rootOfB){
+            if(rank[rootOfA] > rank[rootOfB]){
+                unionSet[rootOfB] = rootOfA;
+            }else if(rank[rootOfA] < rank[rootOfB]){
+                unionSet[rootOfA] = rootOfB;
+            }else{
+                unionSet[rootOfB] = rootOfA;
+                rank[rootOfA] += 1;
+            }
+
+            return 1;
+        }
+
+        return 0;
+    }
+
+    private int find(int[] unionSet, int node){
+        if(unionSet[node] == node) return node;
+
+        unionSet[node] = find(unionSet, unionSet[node]);
+        return unionSet[node];
     }
 }
