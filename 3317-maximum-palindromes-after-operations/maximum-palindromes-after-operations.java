d9d5697e609ad12946fc5c537d76e60713753a26
Abasifreke James <jamesfreky@gmail.com>
2024-02-14 14:04:12 -0500

    Time: 7 ms (73.21%) | Memory: 45.3 MB (32.45%) - LeetSync

--- 3317-maximum-palindromes-after-operations/maximum-palindromes-after-operations.java
+++ 3317-maximum-palindromes-after-operations/maximum-palindromes-after-operations.java
@@ -16,58 +16,32 @@ class Solution {
                while(i <= l/2){
                    // similate getting pair by decreasing totalNumOfPairs by 1;
                }
-               if(l was odd){
-                   // find index of odd and remove. 
-               }
+              
             }
         */
 
         Arrays.sort(words, (a, b) -> a.length() - b.length());
         int[] countMap = new int[26];
-
         for(String word: words){
             for(char c: word.toCharArray()){
                 countMap[c - 'a']++;
             }
         }
 
-        System.out.println(Arrays.toString(countMap));
-
         // extracted pairs and left remainders in map
         int totalPairs = 0;
         for(int i = 0; i < 26; i++){
             totalPairs += countMap[i]/2;
-            countMap[i] = countMap[i]%2;
         }
-
-        System.out.println(totalPairs);
-        System.out.println(Arrays.toString(countMap));
+      
         int maxPals = 0;
         for(String word: words){
             int l = word.length();
-            int matchedL = 0;
-
-            matchedL = l/2;
-             System.out.println("111 matchedL: " + matchedL + " with totalPairs being " + totalPairs + " and word " + word);
+            int matchedL = l/2;
             if(totalPairs < matchedL) break;
 
             totalPairs -= matchedL;
-            // matchedL *= 2;
-            // if(l %2 == 1){
-            //     for(int i = 0; i < 26; i++){
-            //         if(countMap[i] == 1){
-            //             matchedL++;
-            //             countMap[i] = 0;
-            //             break;
-            //         }
-            //     }
-            // }
-             System.out.println("matchedL: " + matchedL + " with L being " + l);
-
-            // if(matchedL == l) {
-                maxPals++;
-            // }
-
+            maxPals++;
         }
 
         return maxPals;
