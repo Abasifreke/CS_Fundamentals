8927072d1250f27bb7f7f462e090d77041963c03
Abasifreke James <jamesfreky@gmail.com>
2024-02-14 14:27:37 -0500

    Time: 5 ms (67.72%) | Memory: 42.5 MB (80.78%) - LeetSync

---
+++ 936-rle-iterator/rle-iterator.java
@@ -0,0 +1,33 @@
+class RLEIterator {
+    private int currIndex;
+    private int[] encoding;
+
+    public RLEIterator(int[] encoding) {
+        this.currIndex = 0;
+        this.encoding = encoding;
+    }
+    
+    public int next(int n) {
+        int agg = 0;
+        while(this.currIndex < this.encoding.length && agg + this.encoding[this.currIndex] < n){
+            agg += this.encoding[this.currIndex];
+            this.encoding[this.currIndex] = 0;
+            this.currIndex += 2;
+        }
+
+        if(this.currIndex >= this.encoding.length) return -1;
+
+        this.encoding[this.currIndex] -= (n - agg);
+        int result = this.encoding[this.currIndex + 1];
+
+        if(this.encoding[this.currIndex] == 0) this.currIndex += 2;
+
+        return result;
+    }
+}
+
+/**
+ * Your RLEIterator object will be instantiated and called as such:
+ * RLEIterator obj = new RLEIterator(encoding);
+ * int param_1 = obj.next(n);
+ */
