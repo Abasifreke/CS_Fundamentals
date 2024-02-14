97a7467066ae8315164c4936219f99999fffc971
Abasifreke James <jamesfreky@gmail.com>
2024-02-14 12:36:18 -0500

    Time: 23 ms (54.57%) | Memory: 50.6 MB (88.37%) - LeetSync

--- 1120-flower-planting-with-no-adjacent/flower-planting-with-no-adjacent.java
+++ 1120-flower-planting-with-no-adjacent/flower-planting-with-no-adjacent.java
@@ -1,51 +1,44 @@
 class Solution {
-//     final int FLOWER_TYPES = 4;
-//     public int[] gardenNoAdj(int n, int[][] paths) {
-//         List<List<Integer>> graph = new ArrayList<>();
+    final int FLOWER_TYPES = 4;
+    public int[] gardenNoAdj(int n, int[][] paths) {
+        List<List<Integer>> graph = new ArrayList<>();
 
-//         for(int i = 0; i <= n; i++){
-//             graph.set(i, new ArrayList<>());
-//         }
-
-//         // create graph
-//         for(int[] path: paths){
-//             graph.get(path[0]).add(path[1]);
-//             graph.get(path[1]).add(path[0]);
-//         }
-
-//         // start from 
-//         int[] planted = new int[n];
-//         for(int i = 1; i <= n; i++){
-//             if(planted[i-1] == 0){
-//                 backtrack(graph, i, planted, n);
-//             }
-//         }
-
-//         return planted;
-//     }
-
-//     private boolean backtrack(List<List<Integer>> graph, int i, int[] planted, int N){
-//         if(i)
-//     }
-    public int[] gardenNoAdj(int N, int[][] paths) {
-        int[] ans = new int[N];
-        HashMap<Integer, List<Integer>> edges = new HashMap<>();
-        HashMap<Integer, Set<Integer>> color = new HashMap<>();
-        for(int i = 1; i <= N; i++) {
-            edges.put(i, new ArrayList<>());
-            color.put(i, new HashSet<>());
+        for(int i = 0; i <= n; i++){
+            graph.add(new ArrayList<>());
         }
-        for(int[] p : paths) {
-            edges.get(p[0]).add(p[1]);
-            edges.get(p[1]).add(p[0]);
+
+        // create graph
+        for(int[] path: paths){
+            graph.get(path[0]).add(path[1]);
+            graph.get(path[1]).add(path[0]);
         }
-        for(int i = 1; i <= N; i++) {
-            int c = 1;
-            while(color.get(i).contains(c)) c++;
-            ans[i-1] = c;
-            for(int k : edges.get(i)) color.get(k).add(c);
+
+        // start from 
+        int[] planted = new int[n+1];
+        backtrack(graph, 1, planted);
+        System.out.println(Arrays.toString(planted));
+        return Arrays.copyOfRange(planted, 1, n+1);
+    }
+
+    private boolean backtrack(List<List<Integer>> graph, int i, int[] planted){
+        if(i == planted.length) return true;
+
+        for(int flower = 1; flower <= FLOWER_TYPES; flower++){
+            if(isSafe(graph, flower, planted, i)){
+                planted[i] = flower;
+                if(backtrack(graph, i+1, planted)) return true;
+                planted[i] = 0;
+            }
         }
-        return ans;
+
+        return false;
+    }
+
+    private boolean isSafe(List<List<Integer>> graph, int flower, int[] planted, int gNum){
+        for(int neighbor: graph.get(gNum)){
+            if(planted[neighbor] == flower) return false;
+        }
+        return true;
     }
 }
 
