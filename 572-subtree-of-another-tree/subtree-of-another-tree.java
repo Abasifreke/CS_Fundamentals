161f7430c70c53f9fe903a2b34551b6e12f9ecba
Abasifreke James <jamesfreky@gmail.com>
2024-02-17 12:04:56 -0500

    Time: 3 ms (28.25%) | Memory: 44.7 MB (10.96%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.java
+++ 572-subtree-of-another-tree/subtree-of-another-tree.java
@@ -33,24 +33,9 @@ class Solution {
         return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
     }
 
-    private boolean isIdentical(TreeNode node1, TreeNode node2) {
-
-        // If any of the nodes is null, then both must be null
-        if (node1 == null || node2 == null) {
-            return node1 == null && node2 == null;
-        }
-
-        // If both nodes are non-empty, then they are identical only if
-        // 1. Their values are equal
-        // 2. Their left subtrees are identical
-        // 3. Their right subtrees are identical
-        return node1.val == node2.val && isIdentical(node1.left, node2.left) && isIdentical(node1.right, node2.right);
-    }
-
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if (p == null || q == null) {
-            return p == null && q == null;
-        }
+        if(p == null && q == null) return true;
+        if(p == null || q == null) return false;
 
         return (p.val == q.val) && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
