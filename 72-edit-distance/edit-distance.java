d3b250b7cc8323b4c0cf39f4576564a6cb22c190
Abasifreke James <jamesfreky@gmail.com>
2024-02-19 19:02:10 -0500

    Time: 3 ms (98.25%) | Memory: 45 MB (54.94%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,38 +1,36 @@
 class Solution {
     public int minDistance(String word1, String word2) {
         int m = word1.length(), n = word2.length();
-        int[][] dp = new int[m+1][n+1];
+        int[][] memo = new int[m][n];
 
-        for(int i = 0; i <= m; i++){
-            dp[i][0] = i;
+        for(int[] a : memo){
+            Arrays.fill(a, -1);
         }
 
-        for(int i = 0; i <= n; i++){
-            dp[0][i] = i;
+        return editDistance(word1, word2, m-1, n-1, memo);
+
+    }
+
+    private int editDistance(String w1, String w2, int i, int j, int[][] memo){
+        if(i < 0){
+            return j + 1;
         }
 
-        for(int i = 1; i <= m; i++){
-            for(int j = 1; j <= n; j++){
-                if(word1.charAt(i-1) == word2.charAt(j-1)){
-                    dp[i][j] = dp[i-1][j-1];
-                }else{
-                    dp[i][j] = Math.min(dp[i-1][j], dp[i][j-1]);
-                    dp[i][j] = Math.min(dp[i][j], dp[i-1][j-1]) + 1;
-                }
-            }
+        if(j < 0){
+            return i + 1;
         }
-        // System.out.println(Arrays.deepToString(dp));
 
-        // /*
-        //    [
-        //         [0, 1, 2, 3], 
-        //         [1, 1, 1, 1],
-        //         [2, 1, 1, 1], 
-        //         [3, 2, 1, 1], 
-        //         [4, 2, 1, 1], 
-        //         [5, 2, 1, 1]]
+        if(memo[i][j] != -1){
+            return memo[i][j];
+        }
 
-        // */
-        return dp[m][n];
+        if(w1.charAt(i) == w2.charAt(j)){
+            memo[i][j] = editDistance(w1, w2, i-1, j-1, memo);
+        }else{
+            memo[i][j] = Math.min(editDistance(w1, w2, i-1, j, memo), editDistance(w1, w2, i, j-1, memo)) ;
+            memo[i][j] = Math.min(memo[i][j], editDistance(w1, w2, i-1, j-1, memo))  + 1;
+        }   
+
+        return memo[i][j];
     }
 }
