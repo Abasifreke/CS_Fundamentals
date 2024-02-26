c98b25512e3102a7b575854056590a6874fc12ca
Abasifreke James <jamesfreky@gmail.com>
2024-02-26 11:39:47 -0500

    Time: 0 ms (100.00%) | Memory: 41.6 MB (7.57%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -15,8 +15,7 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p == null && q == null) return true;
-        if(p == null || q == null) return false;
+        if(p == null || q == null) return p == q;
 
         return (p.val == q.val) && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
