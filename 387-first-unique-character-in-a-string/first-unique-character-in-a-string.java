1622bdcf542e9cbd7559ae5bf88f820669f797a5
Abasifreke James <jamesfreky@gmail.com>
2024-03-28 21:23:56 -0400

    Time: 5 ms (87.45%) | Memory: 44.9 MB (65.42%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
@@ -3,12 +3,11 @@ class Solution {
         // only supporting lowercase letter per constraints.
         int[] charCount = new int[26]; 
 
-        for (char c : s.toCharArray()) {
-            charCount[c-'a']++;
+        for (int i = 0; i < s.length(); i++) {
+            charCount[s.charAt(i)-'a']++;
         }
 
         for (int i = 0; i < s.length(); i++) {
-          
             if (charCount[s.charAt(i) - 'a'] == 1)
                 return i;
         }
